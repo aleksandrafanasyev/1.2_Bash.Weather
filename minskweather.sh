ea95b197644b0057fa73abac49081515be2e4b31
Aleksandr Afanasyev <sanny.afanasyev@gmail.com>
2018-03-31 13:47:08 +0300

    cheking utilities

--- minskweather.sh
+++ minskweather.sh
@@ -2,6 +2,11 @@
 
 source ./config.ini
 
+which wget 1>/dev/null || echo "no wget in " `uname`
+which grep 1>/dev/null || echo "no grep in " `uname`
+which sed 1>/dev/null || echo "no sed in " `uname`
+which sleep 1>/dev/null || echo "no sleep in " `uname`
+
 while true; do
 	tmp=`wget -q -O - www.gismeteo.by/weather-minsk-4248 | grep -o '.*<span class="tab-weather__value_m">.*'`
 	min=`echo "${tmp}" |   grep -o '.*<s' |  sed -e 's/&minus/-/g' | sed -e 's/[^-0-9]//g'`
